298c4d79ce1e0a00e6f90a4ba35405ccc36c0c26
JackMarx <seriousfools@gmail.com>
2017-10-02 19:18:37 -0500

    create

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -3,6 +3,16 @@ class EmployeesController < ApplicationController
     @employees = Employee.all
   end
 
+  def create
+    @employee = Employee.new(
+                             first_name: params[:first_name],
+                             last_name: params[:last_name],
+                             email: params[:email]
+                             )
+    @employee.save
+    render :show
+  end
+
   def show
     @employee = Employee.find(params[:id])
   end
